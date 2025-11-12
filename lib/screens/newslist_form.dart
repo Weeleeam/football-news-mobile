625efbd581dc5902dd6afa9b64b1bb24bc8f96d2
Weeleeam <william.jonnatan@ui.ac.id>
2025-11-12 14:47:34 +0700

    Tutorial 8 done

--- lib/screens/newslist_form.dart
+++ lib/screens/newslist_form.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/widgets/left_drawer.dart';
+import 'dart:convert';
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:football_news/screens/menu.dart';
 
 class NewsFormPage extends StatefulWidget {
-    const NewsFormPage({super.key});
+  const NewsFormPage({super.key});
 
-    @override
-    State<NewsFormPage> createState() => _NewsFormPageState();
+  @override
+  State<NewsFormPage> createState() => _NewsFormPageState();
 }
 
 class _NewsFormPageState extends State<NewsFormPage> {
@@ -26,13 +30,10 @@ class _NewsFormPageState extends State<NewsFormPage> {
   ];
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Scaffold(
       appBar: AppBar(
-        title: const Center(
-          child: Text(
-            'Form Tambah Berita',
-          ),
-        ),
+        title: const Center(child: Text('Form Tambah Berita')),
         backgroundColor: Colors.indigo,
         foregroundColor: Colors.white,
       ),
@@ -42,7 +43,7 @@ class _NewsFormPageState extends State<NewsFormPage> {
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children:[
+            children: [
               // === Title ===
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -105,11 +106,12 @@ class _NewsFormPageState extends State<NewsFormPage> {
                   ),
                   value: _category,
                   items: _categories
-                      .map((cat) => DropdownMenuItem(
-                            value: cat,
-                            child: Text(
-                                cat[0].toUpperCase() + cat.substring(1)),
-                          ))
+                      .map(
+                        (cat) => DropdownMenuItem(
+                          value: cat,
+                          child: Text(cat[0].toUpperCase() + cat.substring(1)),
+                        ),
+                      )
                       .toList(),
                   onChanged: (String? newValue) {
                     setState(() {
@@ -159,41 +161,46 @@ class _NewsFormPageState extends State<NewsFormPage> {
                   padding: const EdgeInsets.all(8.0),
                   child: ElevatedButton(
                     style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.indigo),
+                      backgroundColor: MaterialStateProperty.all(Colors.indigo),
                     ),
-                    onPressed: () {
+                    onPressed: () async {
                       if (_formKey.currentState!.validate()) {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              title: const Text('Berita berhasil disimpan!'),
-                              content: SingleChildScrollView(
-                                child: Column(
-                                  crossAxisAlignment:
-                                      CrossAxisAlignment.start,
-                                  children: [
-                                    Text('Judul: $_title'),
-                                    Text('Isi: $_content'),
-                                    Text('Kategori: $_category'),
-                                    Text('Thumbnail: $_thumbnail'),
-                                    Text('Unggulan: ${_isFeatured ? "Ya" : "Tidak"}'),
-                                  ],
+                        // TODO: Replace the URL with your app's URL
+                        // To connect Android emulator with Django on localhost, use URL http://10.0.2.2/
+                        // If you using chrome,  use URL http://localhost:8000
+                        final response = await request.postJson(
+                          "http://localhost:8000/create-flutter/",
+                          jsonEncode({
+                            "title": _title,
+                            "content": _content,
+                            "thumbnail": _thumbnail,
+                            "category": _category,
+                            "is_featured": _isFeatured,
+                          }),
+                        );
+                        if (context.mounted) {
+                          if (response['status'] == 'success') {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text("News successfully saved!"),
+                              ),
+                            );
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => MyHomePage(),
+                              ),
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text(
+                                  "Something went wrong, please try again.",
                                 ),
                               ),
-                              actions: [
-                                TextButton(
-                                  child: const Text('OK'),
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                ),
-                              ],
                             );
-                          },
-                        );
-                        _formKey.currentState!.reset();
+                          }
+                        }
                       }
                     },
                     child: const Text(
@@ -204,7 +211,7 @@ class _NewsFormPageState extends State<NewsFormPage> {
                 ),
               ),
             ],
-          )
+          ),
         ),
       ),
     );
